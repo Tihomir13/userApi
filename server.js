6518348c433a5a6d478a9950c3f16cdfd05b622e
Tihomir13 <tihomir13n2002@abv.bg>
2024-09-11 22:03:06 +0300

    update   useNewUrlParser: true,          useUnifiedTopology: true,

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const client = new MongoClient(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const client = new MongoClient(process.env.MONGODB_URI, {});
 
 let db;
 async function connectDB() {
